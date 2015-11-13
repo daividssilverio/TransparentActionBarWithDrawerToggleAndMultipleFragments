259e4eaad3685cb08b35d896aa0d378a617cb835
Daivid Silverio <daivid.silverio@gmail.com>
2015-11-13 19:07:17 -0300

    right from the samples

---
+++ app/src/main/java/com/ddapps/moo/Fragment1.java
@@ -0,0 +1,19 @@
+package com.ddapps.moo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by daividsilverio on 11/13/15.
+ */
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_1, container, false);
+    }
+}
